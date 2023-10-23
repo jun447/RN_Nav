c1309fbf31295aa75c32476fad8c94b9e30f2863
DevJun <72307789+jun447@users.noreply.github.com>
2023-10-23 19:24:59 +0000

    Added States and Props

--- AwesomeNavigation/screens/About.jsx
+++ AwesomeNavigation/screens/About.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 
 const About = ({navigation,route}) => {
   const {takeData} = route.params
+  const {myNumbr} = route.params
   return (
     <View>
       <Text>About</Text>
       <Text>{takeData}</Text>
+      <Text>{myNumbr}</Text>
+      <Text>{route.params.value}</Text>
     </View>
   )
 }
--- AwesomeNavigation/screens/Home.jsx
+++ AwesomeNavigation/screens/Home.jsx
@@ -1,16 +1,27 @@
 import { Pressable, StyleSheet, Text, View} from 'react-native'
-import React from 'react'
+import React ,{useState} from 'react'
 
 const Home = ({navigation}) => {
+ const [myGpa, setMyGpa] = useState(3.5)
+
+
   return (
     <View>
       <Text>Home</Text>
       {/* create pressable to go to about screen */}
       <Pressable 
-        onPress={() => navigation.navigate('About',{takeData: 'Hello from Home'})}
+        onPress={() => navigation.navigate('About',{takeData: 'Hello from Ghori',myNumbr: 321,value:myGpa})}
         style={styles.button}
       >
         <Text>Go to About</Text>
+      </Pressable>
+      {/* create pressable to console MyGpa variable */}
+      <Pressable 
+        onPress={() => { console.log(myGpa); setMyGpa(myGpa+1); }}
+        style={styles.button}
+      >
+        <Text>Console MyGpa</Text>
+        <Text>{myGpa}</Text>
       </Pressable>
     </View>
   )
