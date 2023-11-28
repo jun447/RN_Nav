4107c32bd796b905abe3652c40b375f6ed76b0e0
DevJun <72307789+jun447@users.noreply.github.com>
2023-11-28 11:58:03 +0000

    projects starter done

---
+++ foodDelivery/app/_layout.tsx
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router";
+
+
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "index",
+};
+
+export default function RootLayoutNav() {
+  return (
+    <Stack>
+      {/* {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+        <Stack.Screen name="index" />
+    </Stack>
+  );
+}
---
+++ foodDelivery/constants/Colors.ts
@@ -0,0 +1,24 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+  primary: '#20E1b2',
+  lightGray: '#f5f5f5',
+  grey: '#808080',
+  mediumGray: '#a9a9a9',
+  green: '#437920',
+};
