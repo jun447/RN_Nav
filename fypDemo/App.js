352797531f808051292eaeaba65024fc4578a189
DevJun <72307789+jun447@users.noreply.github.com>
2023-12-03 13:22:26 +0000

    Added tab navigation

--- fypDemo/App.js
+++ fypDemo/App.js
@@ -1,11 +1,14 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TabNavigation from './App/Navigation/TabNavigation';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <TabNavigation />
+      </NavigationContainer>
     </View>
   );
 }
@@ -14,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
--- fypDemo/App/Navigation/TabNavigation.jsx
+++ fypDemo/App/Navigation/TabNavigation.jsx
@@ -1,13 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Home from "../Screens/Home";
+import Fav from "../Screens/Fav";
+import Profile from "../Screens/Profile";
+import Search from "../Screens/Search";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const TabNavigation = () => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#e91e63",
+        tabBarInactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Favourite" component={Fav} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Search" component={Search} />
+
     </Tab.Navigator>
   );
 };
